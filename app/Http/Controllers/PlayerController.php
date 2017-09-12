45d970cde7ede53d9cf5e2e800797061391a6bfd
Julien <schmittounet@gmail.com>
2017-09-12 22:32:57 +0200

    Try on request to find the bests players

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -1,84 +1,101 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
-class PlayerController extends Controller 
-{
+use App\Player;
+use Illuminate\Support\Facades\DB;
 
-  /**
-   * Display a listing of the resource.
-   *
-   * @return Response
-   */
-  public function index()
-  {
+class PlayerController extends Controller{
     
-  }
-
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return Response
-   */
-  public function create()
-  {
     
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @return Response
-   */
-  public function store()
-  {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index(){
     
-  }
-
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function show($id)
-  {
+    }
     
-  }
-
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function edit($id)
-  {
+    /**
+     * Return Players with the best win rates
+     */
+    public function getBestPlayers(){
+        // TODO : convert this request to Eloquent ?
+        //
+        // SELECT  `battletag`
+        //     , COUNT(1) AS total_games
+        //     , SUM(CASE WHEN win = 1 THEN 1 ELSE 0 END) AS total_win
+        //     , COUNT(1)-SUM(CASE WHEN win = 1 THEN 1 ELSE 0 END) AS total_lose
+        //     , (SUM(CASE WHEN win = 1 THEN 1 ELSE 0 END)/COUNT(1))*100 AS percent_win
+        // FROM    `participations`, `players`
+        // WHERE 	`participations`.`player_id` = `players`.`id`
+        // GROUP BY `player_id`
+        // ORDER BY total_games DESC
+    }
     
-  }
-
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function update($id)
-  {
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create(){
     
-  }
-
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function destroy($id)
-  {
+    }
     
-  }
-  
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store(){
+    
+    }
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function show($id){
+    
+    }
+    
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function edit($id){
+    
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function update($id){
+    
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function destroy($id){
+    
+    }
+    
 }
 
 ?>
