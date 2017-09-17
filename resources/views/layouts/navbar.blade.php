8d9c580abd7c1ee4e3378db10b28acb84362fddb
Julien <schmittounet@gmail.com>
2017-09-17 10:50:11 +0200

    Added margin bottom to navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
     <div class="container">
         <a class="navbar-brand" href="{{url('/')}}">
             <img src="{{URL::asset('/images/nexus.png')}}" width="30" height="30" class="d-inline-block align-top" alt="">
