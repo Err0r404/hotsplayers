867374c6982526d6e358ebdc7f0c3acc9961ec03
Julien Schmitt <julien.schmitt@imaios.com>
2017-09-26 09:44:01 +0200

    Fix last game id retrieve and added a retry system when scrapping hotsapi

--- app/Console/Commands/ScrapApi.php
+++ app/Console/Commands/ScrapApi.php
@@ -64,12 +64,22 @@ class ScrapApi extends Command
             $lastGame = DB::table('games')->latest('api_id')->first();
 
             // If DB is emtpy start at 0
-            $lastGameId = (sizeof($lastGame) == 0) ? 0 : $lastGame->id;
+            $lastGameId = (sizeof($lastGame) == 0) ? 0 : $lastGame->api_id;
 
             // Get data from API starting from the last Game registered in DB
             $uri      = $baseUri . "/replays";
             $params   = "?min_id=" . ($lastGameId+1);
-            $response = Request::get($uri . $params)->send();
+            $retry    = 0;
+            
+            do{
+                $response = Request::get($uri . $params)->send();
+                
+                if($response->code == '200'){
+                    $retry++;
+                    sleep(1);
+                }
+                
+            }while($response->code != 200 && $retry < 5);
 
             // If API returned code 200
             if($response->code == '200'){
@@ -86,98 +96,110 @@ class ScrapApi extends Command
                     $apiGameDate   = $replay->game_date;
                     $apiGameLength = $replay->game_length;
                     $apiMapName    = $replay->game_map;
-
-                    // Get additional data about the replay
-                    $additionalUri      = $uri . "/" . $apiId;
-                    $additionalResponse = Request::get($additionalUri)->send();
-
-                    // If API returned code 200
-                    if($additionalResponse->code == 200){
-                        // Wrapper
-                        $apiPlayers = $additionalResponse->body->players;
-
-                        // Enable mass assignment
-                        Map::unguard();
-                        Game::unguard();
-                        Player::unguard();
-                        Hero::unguard();
-                        Participation::unguard();
-
-                        // Get or create Map
-                        $map = Map::firstOrCreate(
-                            array('name' => $apiMapName)
-                        );
-    
-                        // Update the column updated_at
-                        $map->touch();
+                    
+                    if($apiMapName != null){
+                        // Get additional data about the replay
+                        $additionalUri  = $uri . "/" . $apiId;
+                        $addtionalRetry = 0;
                         
-                        //$this->info("Map : $map");
-
-                        // Get or create Game
-                        $game = Game::firstOrCreate(
-                            ['api_id' => $apiId],
-                            ['type' => $apiGameType, 'length' => $apiGameLength, 'date' => $apiGameDate, 'map_id' => $map->id]
-                        );
-                        
-                        // Update the column updated_at
-                        $game->touch();
-
-                        //$this->info("Game : $game");
-
-                        // Loop trough all players in the replay
-                        foreach ($apiPlayers as $apiPlayer) {
-                            // Wrappers
-                            $apiBattletag  = $apiPlayer->battletag;
-                            $apiBlizzardId = $apiPlayer->blizz_id;
-                            $apiHeroName   = $apiPlayer->hero;
-                            $apiWin        = $apiPlayer->winner;
-
-                            // Get or create Player
-                            $player = Player::firstOrCreate(
-                                ['blizzard_id' => $apiBlizzardId],
-                                ['battletag' => $apiBattletag]
-                            );
+                        do{
+                            $additionalResponse = Request::get($additionalUri)->send();
                             
-                            // Update the column updated_at
-                            $player->touch();
+                            if($additionalResponse->code != '200'){
+                                $addtionalRetry++;
+                                sleep(1);
+                            }
                             
-                            //$this->info("Player : $player");
-
-                            // Get or create Hero
-                            $hero = Hero::firstOrCreate(
-                                ['name' => $apiHeroName]
+                        }while($additionalResponse->code != '200' && $addtionalRetry < 5);
+    
+                        // If API returned code 200
+                        if($additionalResponse->code == 200){
+                            // Wrapper
+                            $apiPlayers = $additionalResponse->body->players;
+        
+                            // Enable mass assignment
+                            Map::unguard();
+                            Game::unguard();
+                            Player::unguard();
+                            Hero::unguard();
+                            Participation::unguard();
+        
+                            // Get or create Map
+                            $map = Map::firstOrCreate(
+                                array('name' => $apiMapName)
                             );
-    
+        
                             // Update the column updated_at
-                            $hero->touch();
-    
-                            //$this->info("Hero : $hero");
-
-                            // Get or create Participation
-                            $participation = Participation::firstOrCreate(
-                                ['hero_id' => $hero->id, 'player_id' => $player->id, 'game_id' => $game->id],
-                                ['win' => $apiWin]
+                            $map->touch();
+        
+                            //$this->info("Map : $map");
+        
+                            // Get or create Game
+                            $game = Game::firstOrCreate(
+                                ['api_id' => $apiId],
+                                ['type' => $apiGameType, 'length' => $apiGameLength, 'date' => $apiGameDate, 'map_id' => $map->id]
                             );
-    
+        
                             // Update the column updated_at
-                            $participation->touch();
-    
-                            //$this->info("Participation : $participation");
+                            $game->touch();
+        
+                            //$this->info("Game : $game");
+        
+                            // Loop trough all players in the replay
+                            foreach ($apiPlayers as $apiPlayer) {
+                                // Wrappers
+                                $apiBattletag  = $apiPlayer->battletag;
+                                $apiBlizzardId = $apiPlayer->blizz_id;
+                                $apiHeroName   = $apiPlayer->hero;
+                                $apiWin        = $apiPlayer->winner;
+            
+                                // Get or create Player
+                                $player = Player::firstOrCreate(
+                                    ['blizzard_id' => $apiBlizzardId],
+                                    ['battletag' => $apiBattletag]
+                                );
+            
+                                // Update the column updated_at
+                                $player->touch();
+            
+                                //$this->info("Player : $player");
+            
+                                // Get or create Hero
+                                $hero = Hero::firstOrCreate(
+                                    ['name' => $apiHeroName]
+                                );
+            
+                                // Update the column updated_at
+                                $hero->touch();
+            
+                                //$this->info("Hero : $hero");
+            
+                                // Get or create Participation
+                                $participation = Participation::firstOrCreate(
+                                    ['hero_id' => $hero->id, 'player_id' => $player->id, 'game_id' => $game->id],
+                                    ['win' => $apiWin]
+                                );
+            
+                                // Update the column updated_at
+                                $participation->touch();
+            
+                                //$this->info("Participation : $participation");
+                            }
+        
+                            $this->info("Replay #$apiId done");
+        
+                            // Disable mass assignment
+                            Map::reguard();
+                            Game::reguard();
+                            Player::reguard();
+                            Hero::reguard();
+                            Participation::reguard();
                         }
-
-                        $this->info("Replay #$apiId done");
-
-                        // Disable mass assignment
-                        Map::reguard();
-                        Game::reguard();
-                        Player::reguard();
-                        Hero::reguard();
-                        Participation::reguard();
-                    }
-                    else{
-                        $this->error("API didn't response correctly");
-                        $this->error("URI : $additionalUri");
-                        $this->error("Response Code : #".$additionalResponse->code);
+                        else{
+                            $this->error("API didn't response correctly");
+                            $this->error("URI : $additionalUri");
+                            $this->error("Response Code : #".$additionalResponse->code);
+                        }
                     }
                 }
             }
