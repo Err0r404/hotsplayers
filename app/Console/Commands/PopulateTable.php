6f22d86f44c3441e8a2fc2a2f93195c5a5071bbb
Julien Schmitt <julien.schmitt@imaios.com>
2017-11-17 11:00:33 +0100

    Console script to populate each table

--- app/Console/Commands/PopulateTable.php
+++ app/Console/Commands/PopulateTable.php
@@ -57,7 +57,7 @@ class PopulateTable extends Command
             case "games":
                 $this->line("START populate table $tableName");
                 
-                $sql = "INSERT 	INTO `custom_games` (`api_id`, `type`, `length`, `date`, `map_id`, `created_at`, `updated_at`)
+                $sql = "INSERT  INTO `hotsapi`.`custom_games` (`api_id`, `type`, `length`, `date`, `map_id`, `created_at`, `updated_at`)
                         SELECT	`replays`.`id`,
                                 `replays`.`game_type`,
                                 `replays`.`game_length`,
@@ -65,8 +65,8 @@ class PopulateTable extends Command
                                 `custom_maps`.`id`,
                                 NOW(),
                                 NOW()
-                        FROM 	`replays`,
-                                `custom_maps`
+                        FROM 	`hotsapi`.`replays`,
+                                `hotsapi`.`custom_maps`
                         WHERE 	`replays`.`game_map` = `custom_maps`.`name`
                         ON DUPLICATE KEY UPDATE `updated_at` = NOW()";
     
@@ -77,11 +77,11 @@ class PopulateTable extends Command
             case "heroes":
                 $this->line("START populate table $tableName");
     
-                $sql = "INSERT 	INTO `custom_heroes` (`name`, `created_at`, `updated_at`)
+                $sql = "INSERT  INTO `hotsapi`.`custom_heroes` (`name`, `created_at`, `updated_at`)
                         SELECT 	DISTINCT(`hero`),
                                 NOW(),
                                 NOW()
-                        FROM 	`players`
+                        FROM 	`hotsapi`.`players`
                         ON DUPLICATE KEY UPDATE `updated_at` = NOW()";
     
                 DB::insert($sql);
@@ -91,19 +91,70 @@ class PopulateTable extends Command
             case "maps":
                 $this->line("START populate table $tableName");
     
-                $sql = "INSERT 	INTO `custom_maps` (`name`, `created_at`, `updated_at`)
+                $sql = "INSERT  INTO `hotsapi`.`custom_maps` (`name`, `created_at`, `updated_at`)
                         SELECT 	DISTINCT(`game_map`),
                                 NOW(),
                                 NOW()
-                        FROM 	`replays`
+                        FROM 	`hotsapi`.`replays`
                         WHERE	`game_map` IS NOT NULL
                         ON DUPLICATE KEY UPDATE `updated_at` = NOW()";
     
                 DB::insert($sql);
-    
+                
                 $this->line("END populate table $tableName");
                 break;
             case "participations":
+                $this->line("WARNING this will take several hours");
+                sleep(3);
+    
+                $limit      = 10000;
+                $offset     = $offsetOption;
+                $maxPlayers = $hotsapi->table('players')->count();
+                $maxHeroes  = $hotsapi->table('custom_heroes')->count();
+                
+                for($heroId = 1; $heroId <= $maxHeroes; $heroId++){
+                    do{
+                        // Starting time
+                        $timeStart = microtime(true);
+    
+                        $this->line("Running request with \$offset => $offset and hero_id = $heroId");
+    
+                        $sql = "INSERT  INTO `hotsapi`.`custom_participations` (`hero_id`, `player_id`, `game_id`, `win`, `created_at`, `updated_at`)
+                                SELECT 	`custom_heroes`.`id` AS hero_id,
+                                        `custom_players`.`id` AS player_id,
+                                        `custom_games`.`id` AS game_id,
+                                        `players`.`winner`,
+                                        NOW() AS created_at,
+                                        NOW() AS updated_at
+                                FROM 	`hotsapi`.`players`
+                                        INNER JOIN `hotsapi`.`custom_heroes`  ON `players`.`hero`      = `custom_heroes`.`name`
+                                        INNER JOIN `hotsapi`.`custom_players` ON `players`.`blizz_id`  = `custom_players`.`blizzard_id`
+                                        INNER JOIN `hotsapi`.`custom_games`   ON `players`.`replay_id` = `custom_games`.`api_id`
+                                WHERE  `custom_heroes`.`id` = $heroId
+                                LIMIT 	$limit OFFSET $offset
+                                ON DUPLICATE KEY UPDATE `updated_at` = NOW()";
+    
+                        $res = DB::insert($sql);
+                        
+                        $offset += $limit;
+    
+                        // Ending time
+                        $timeEnd = microtime(true);
+    
+                        // Execution time
+                        $time = round(($timeEnd - $timeStart),0);
+    
+                        // Convert seconds to a human readable format
+                        $c = new Controller();
+                        $time = $c->secondsToHumanReadableString($time);
+    
+                        $this->info("Insert executed in $time");
+    
+                        usleep(250000);
+    
+                    }while($offset < $maxPlayers || DB::getPdo()->lastInsertId() != 0);
+                }
+                
                 break;
             case "players":
                 $this->line("WARNING this will take several hours");
@@ -147,7 +198,7 @@ class PopulateTable extends Command
         
                     $this->info("Insert executed in $time");
         
-                    usleep(500000);
+                    usleep(250000);
         
                 } while ($offset <= $max);
 
